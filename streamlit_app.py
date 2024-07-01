fc7723e503f5d99711212f020ceb9ce6ac719e5f
Murat Gocer <mrtgocer@gmail.com>
2024-07-01 10:46:49 +0200

    Update streamlit_app.py with new changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,6 @@ st.write('Aşağıdaki bilgileri girerek Titanic gemisinde hayatta kalma olasıl
 # Giriş alanları
 Pclass = st.selectbox('Pclass (Bilet Sınıfı)', [1, 2, 3])
 Sex = st.selectbox('Sex (Cinsiyet)', ['male', 'female'])
-Age = st.number_input('Age (Yaş)', min_value=0, max_value=100, value=30)
 SibSp = st.number_input('SibSp (Kardeş/Equyaşmak)', min_value=0, max_value=10, value=0)
 Parch = st.number_input('Parch (Ebeveyn/Çocuk)', min_value=0, max_value=10, value=0)
 Fare = st.number_input('Fare (Bilet Ücreti)', min_value=0.0, value=50.0)
@@ -33,7 +32,7 @@ Embarked_S = 1 if Embarked == 'S' else 0
 
 # Tahmin butonu
 if st.button('Predict'):
-    input_data = np.array([[Pclass, Sex, Age, SibSp, Parch, Fare, Embarked_C, Embarked_Q, Embarked_S]])
+    input_data = np.array([[Pclass, Sex, SibSp, Parch, Fare, Embarked_C, Embarked_Q, Embarked_S]])
     prediction = model.predict(input_data)
     prediction_prob = model.predict_proba(input_data)
     st.write(f'Hayatta kalma olasılığınız: %{prediction_prob[0][1] * 100:.2f}')
